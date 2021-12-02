ffe3f1f965d82230d7a33558784b0770bbd7a148
brunoleonel <bruno.leonel@hotmail.com>
2021-12-02 19:23:09 -0300

    Inicio

---
+++ PROGCOB01.cob
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB01.
+      **************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTHOR = BRUNO LEONEL 
+      * OBJETIVO = HELLO WORLD
+      * DATA = XX/XX/XXXX
+      **************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+           DISPLAY 'HELLO WORLD'.
+           STOP RUN.
