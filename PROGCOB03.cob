8b517c30181ed2eba1adafc93c785d719823e7ea
Bruno Leonel <bruno_leonels@hotmail.com>
2021-12-08 23:20:39 -0300

    Aulas 3 e 4

---
+++ PROGCOB03.cob
@@ -0,0 +1,20 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB03.
+      **************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTHOR = BRUNO LEONEL 
+      * OBJETIVO = ATRIBUICAO DE VARIAVEIS NV 1, 2 (ESTRUTURADAS)
+      * USANDO DATA DO SISTEMA
+      * DATA = XX/XX/XXXX
+      **************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WRK-DATA.
+           02 WRK-ANO PIC 9(04) VALUE ZEROS.
+           02 WRK-MES PIC 9(02) VALUE ZEROS.
+           02 WRK-DIA PIC 9(02) VALUE ZEROS.
+       PROCEDURE DIVISION.
+           ACCEPT WRK-DATA FROM DATE YYYYMMDD.
+           DISPLAY WRK-DIA '/' WRK-MES '/' WRK-ANO.
+           STOP RUN.
